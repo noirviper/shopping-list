6aabefa7b6e4fb04fc342c3d511612a83d2bfb86
Ryan Johnson <christopher.r.johnson2@gmail.com>
2019-12-07 22:20:48 -0800

    adding finished project to github

---
+++ index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let mainContainer = $('.container');
+let listItem = $('#shopping-list-entry');
+let shoppingList = $('.shopping-list');
+
+mainContainer.on('click keydown', 'button', function(event){
+    event.preventDefault();
+    if($(this).attr('type') === 'submit') {
+        addItem($(listItem).val());
+    } else if ($(this).hasClass("shopping-item-toggle")) {
+        $(this).parent().parent().find(".shopping-item").toggleClass('shopping-item__checked');
+    } else if ($(this).hasClass("shopping-item-delete")) {
+        $(this).parent().parent().remove();
+    }
+    
+})
+
+function addItem(item) {
+    $(shoppingList).append(function(){
+        return `<li>
+        <span class="shopping-item">${item}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+    });
+}
